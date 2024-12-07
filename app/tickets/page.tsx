22c87260385e3b9dba395c3f7395a09a30b156a5
Mehdi Ahmadi <metti799@gmail.com>
2024-12-07 11:08:33 +0330

    feat : add pagination

--- app/tickets/page.tsx
+++ app/tickets/page.tsx
@@ -1,18 +1,37 @@
+import Pagination from "@/components/Pagination";
 import { buttonVariants } from "@/components/ui/button";
 import prisma from "@/prisma/db";
 import Link from "next/link";
 import DataTable from "./DataTable";
 
-const Tickets = async () => {
-  const tickets = await prisma.ticket.findMany();
+interface SearchParams {
+  page: string;
+}
 
+const Tickets = async ({ searchParams }: { searchParams: SearchParams }) => {
+  
+  const pageSize = 10;
+  const page = parseInt(searchParams.page) || 1;
+  const ticketCount = await prisma.ticket.count();
+  
+  const tickets = await prisma.ticket.findMany({
+    take : pageSize,
+    skip : (page - 1) * pageSize
+  });
   return (
     <div>
       <Link
         href={"/tickets/new"}
         className={buttonVariants({ variant: "default" })}
-      >New Ticket</Link>
+      >
+        New Ticket
+      </Link>
       <DataTable tickets={tickets} />
+      <Pagination
+        itemCount={ticketCount}
+        pageSize={pageSize}
+        currentPage={page}
+      />
     </div>
   );
 };
