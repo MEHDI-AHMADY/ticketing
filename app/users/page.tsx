42f1c9137fb5d556cce04f1c43189e3d39b5b96d
Mehdi Ahmadi <metti799@gmail.com>
2024-12-08 18:27:23 +0330

    feat : add userTable

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,9 +1,18 @@
+import { buttonVariants } from "@/components/ui/button";
 import UserForm from "@/components/UserForm"
+import prisma from "@/prisma/db"
+import Link from "next/link";
+import DataTableSimple from "./DataTableSimple"
 
-const Users = () => {
+const Users = async () => {
+
+  const users = await prisma?.user.findMany();
   return (
     <div>
-      <UserForm />
+      <Link href={`/users/new`} className={`${buttonVariants({variant : "default"})}`}>
+        Create User
+      </Link>
+      <DataTableSimple users={users}/>
     </div>
   )
 }
