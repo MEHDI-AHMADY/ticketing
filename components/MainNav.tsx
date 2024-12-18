7dd2a6234c4f5a7260c556b470d9bfad4bc64e0b
Mehdi Ahmadi <metti799@gmail.com>
2024-12-18 16:00:12 +0330

    feat : implement Authentication with nextAuth

--- components/MainNav.tsx
+++ components/MainNav.tsx
@@ -1,14 +1,22 @@
+import options from "@/app/api/auth/[...nextauth]/options";
+import { getServerSession } from "next-auth";
 import Link from "next/link";
 import MainNavLinks from "./MainNavLinks";
 import ToggleMode from "./ToggleMode";
 
-const MainNav = () => {
+const MainNav = async () => {
+  const session = await getServerSession(options);
+
   return (
     <div className="flex justify-between">
       <MainNavLinks />
 
       <div className="flex items-center gap-2">
-        <Link href="/">Logout</Link>
+        {session ? (
+          <Link href="/api/auth/signout?callbackUrl=/">Logout</Link>
+        ) : (
+          <Link href="/api/auth/signin">Login</Link>
+        )}
         <ToggleMode />
       </div>
     </div>
